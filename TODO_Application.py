9fe3fdf97e3bfb593942f0ecd44b7c4f87eb379b
Salman Raza <SNAQVI@ibs-b.hu>
2020-11-19 13:41:24 +0100

    Checking

--- TODO_Application.py
+++ TODO_Application.py
@@ -1,1 +1,19 @@
-#ABC
+import sys
+
+print(sys.argv)
+
+if len(sys.argv) == 1:
+    print("")
+    print("$ todo")
+    print("")
+    print("Command Line Todo application")
+    print("=============================")
+    print("")
+    print("Command line arguments:")
+    print("      -l   Lists all the tasks")
+    print("      -a   Adds a new task")
+    print("      -r   Removes a task")
+    print("      -c   Completes a task")
+elif len(sys.argv) == 2 and sys.argv[1] == "-l":
+    print("")
+    print("List All Elements")
